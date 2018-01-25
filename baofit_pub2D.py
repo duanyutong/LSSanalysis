73a70f0898ebab3d30abbd1a931c3cf0ed8f9350
Duan Yutong <givoltage@gmail.com>
2018-01-24 20:14:38 -0500

    fixed errors of ajr's latest public version which prevents the code from running with correct setup

--- baofit_pub2D.py
+++ baofit_pub2D.py
@@ -1,4 +1,4 @@
-Hdir = '/Users/ashleyross/DR12/'
+Hdir = '/home/dyt/analysis_data/dr12/'
 import numpy as np
 from numpy import zeros,dot
 from numpy.linalg import pinv
@@ -803,7 +803,7 @@ if __name__ == '__main__':
 	min = 50.
 	max = 150. #the minimum and maximum scales to be used in the fit
 	maxb = 80. #the maximum scale to be used to set the bias prior
-	dir = '/Users/ashleyross/DR12/Ross_2016_COMBINEDDR12/' #change to wherever the data is
+	dir = '/home/dyt/LSSanalysis/Ross_2016_COMBINEDDR12/' #change to wherever the data is
 	ft = 'Ross_2016_COMBINEDDR12_'
 	zb = 'zbin3_' #change number to change zbin
 	binc = 0 #change number to change bin center
